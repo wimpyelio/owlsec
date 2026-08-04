9233bfbc2f6968ef513ebbf7d019fffcf6387028
Aditya Patel <153438547+wimpyelio@users.noreply.github.com>
2026-08-04 02:29:57 +0000

    Add nitro preset for Vercel to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,23 @@
+// import { defineConfig } from "vite";
+// import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+// import viteReact from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// import tsConfigPaths from "vite-tsconfig-paths";
+
+// export default defineConfig({
+//   plugins: [
+//     tsConfigPaths({ projects: ["./tsconfig.json"] }),
+//     tailwindcss(),
+//     tanstackStart({
+//       server: { entry: "server" },
+//     }),
+//     viteReact(),
+//   ],
+// });
+
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
@@ -11,6 +29,7 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
+    nitro({ preset: "vercel" }),
     viteReact(),
   ],
 });
